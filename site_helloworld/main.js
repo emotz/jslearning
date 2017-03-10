67b5ae63b0e3bbde62f2bb89ffcdcb1e81252878
MnZrK <mnzrka@gmail.com>
2017-03-11 05:42:30 +0700

    added sample for simple node-based website

---
+++ site_helloworld/main.js
@@ -0,0 +1,14 @@
+'use strict';
+function log(msg) { console.log(msg); }
+function assert(pred) { log(pred ? "Passed." : "Failed."); }
+//////////////////////////////////////////////////
+
+log("Starting our first site!");
+
+let http = require('http');
+
+let server = http.createServer(function(request, response) {
+    log("Received a request for url \"" + request.url + "\"");
+    response.write("<html><body>Hello world</body></html>");
+    response.end();
+}).listen(8080);
